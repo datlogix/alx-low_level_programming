1d85122967919fdd4edbda9d4a04f45b21bb2a6f
Douglas Ayitey <douglasayitey28@gmail.com>
2023-02-21 17:19:44 +0000

    putchar correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	char str[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	int count, sz;
 
 	sz = sizeof(str) / sizeof(int);
